2d4386260870d17f3e85f25223b6dd8cdf994968
ennbou <ennbou@yandex.com>
2024-01-06 11:50:36 +0100

    Initial Commit

---
+++ MelonMod/MelonModApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelonModApp.swift
+//  MelonMod
+//
+//  Created by ENNBOU on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MelonModApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
